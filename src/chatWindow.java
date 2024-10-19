9b7410880296b2919d499ab07d4aca0a61e8bfb2
BW404 <taj.darsana@gmail.com>
2024-10-19 23:36:27 +0600

    Refactor window titles and add GUB branding

--- src/chatWindow.java
+++ src/chatWindow.java
@@ -2,7 +2,7 @@ import java.awt.Color;
 import javax.swing.JFrame;
 public class chatWindow extends JFrame {
     public chatWindow() {
-        this.setTitle("Dino Chat");
+        this.setTitle("GUB Chat");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(1177, 960);
         this.setVisible(true);
--- src/loginWindow.java
+++ src/loginWindow.java
@@ -1,4 +1,6 @@
 import java.awt.Color;
+import java.awt.Font;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -10,35 +12,67 @@ public class loginWindow extends JFrame {
     private JPasswordField passwordField;
     private JButton loginButnButton;
     public loginWindow() {  
-        this.setTitle("Dino Chat Login"); 
+        this.setTitle("GUB Chat Login"); 
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(400, 500);
         this.setVisible(true);
         this.getContentPane().setBackground(new Color(0X1C1D22));System.out.println("Login Window");
+        this.setLayout(null);
+        setLocationRelativeTo(null);
 
-        // this.add(new JLabel("Username:"));
-        // this.usernameField = new JTextField();
-        // this.add(this.usernameField);
+        // gub logo
+        JLabel gubLabel = new JLabel(new ImageIcon("src\\img\\gub_logo.png"));
+        gubLabel.setBounds(100, 20, 200, 200);
+        this.add(gubLabel);
 
+        // Username Label
         JLabel usernamLabel = new JLabel("Username:");
+        usernamLabel.setBounds(150, 150, 300, 50);
+        usernamLabel.setFont(new Font("Comic Sans MS", Font.PLAIN, 20));
         usernamLabel.setForeground(Color.WHITE);
-        usernamLabel.setBounds(50, 100, 300, 50);
         this.add(usernamLabel);
 
+        // Username Field
+        usernameField = new JTextField();
+        usernameField.setBounds(100, 200, 200, 30);
+        usernameField.setFont(new Font("Dubai", Font.PLAIN, 14));
+        usernameField.setForeground(Color.WHITE);
+        usernameField.setBackground(new Color(0X1C1D22));
+        usernameField.setCaretColor(Color.WHITE);
+        this.add(usernameField);
+
+
+        // Password Label
+        JLabel passwordLabel = new JLabel("Password:");
+        passwordLabel.setBounds(150, 225, 300, 50);
+        passwordLabel.setFont(new Font("Comic Sans MS", Font.PLAIN, 20));
+        passwordLabel.setForeground(Color.WHITE);
+        this.add(passwordLabel);
+
+        // Password Field
+        passwordField = new JPasswordField();
+        passwordField.setBounds(100, 275, 200, 30);
+        passwordField.setFont(new Font("Dubai", Font.PLAIN, 14));
+        passwordField.setForeground(Color.WHITE);
+        passwordField.setBackground(new Color(0X1C1D22));
+        passwordField.setCaretColor(Color.WHITE);
+        this.add(passwordField);
 
 
 
 
 
+        
 
-
-
-
-
-
-
-
+        // Revalidate and repaint to update the layout
+        this.revalidate();
+        this.repaint();
+    
+        // Make the frame visible after adding components
+        this.setVisible(true);
 
     }
+
+
 
 }

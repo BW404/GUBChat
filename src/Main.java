b39fe369aaef731e29c20b01e33fca50db46a168
BW404 <taj.darsana@gmail.com>
2024-10-19 22:38:50 +0600

    Refactor Main class and remove instantiation of chatWindow

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
           
         loginWindow LoginWindow = new loginWindow();
-        chatWindow ChatWindow=  new chatWindow();
+        // chatWindow ChatWindow=  new chatWindow();
 
 
     }
--- src/loginWindow.java
+++ src/loginWindow.java
@@ -1,13 +1,44 @@
 import java.awt.Color;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
 
 public class loginWindow extends JFrame {
-    public loginWindow() {
+    private JTextField usernameField;
+    private JPasswordField passwordField;
+    private JButton loginButnButton;
+    public loginWindow() {  
         this.setTitle("Dino Chat Login"); 
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(400, 500);
         this.setVisible(true);
         this.getContentPane().setBackground(new Color(0X1C1D22));System.out.println("Login Window");
+
+        // this.add(new JLabel("Username:"));
+        // this.usernameField = new JTextField();
+        // this.add(this.usernameField);
+
+        JLabel usernamLabel = new JLabel("Username:");
+        usernamLabel.setForeground(Color.WHITE);
+        usernamLabel.setBounds(50, 100, 300, 50);
+        this.add(usernamLabel);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 
 }

20826ca6b862bd3fa70dcecbfaee798e57432453
BW404 <taj.darsana@gmail.com>
2024-11-29 21:11:30 +0600

    Update: 2024-11-29 21:11:30

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        
-        // signupWindow signupWindow = new signupWindow();
-        @SuppressWarnings("unused")
-        loginWindow LoginWindow = new loginWindow();
-        // ChatWindow ChatWindow=  new ChatWindow();
-
-
+        new ChatWindow("TestUser").setVisible(true);
     }
 }

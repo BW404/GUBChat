5003101bf828f358b69063c9e9660e92d59b3183
BW404 <taj.darsana@gmail.com>
2024-11-29 19:44:46 +0600

    Update: 2024-11-29 19:44:46

--- src/chatWindow.java
+++ src/chatWindow.java
@@ -76,9 +76,9 @@ public class ChatWindow extends JFrame {
         contactListModel.addElement("Jane Smith");
         contactListModel.addElement("Alice Johnson");
         contactList = new JList<>(contactListModel);
-        contactList.setBackground(new Color(0x1C1D22));
+        contactList.setBackground(new Color(0x084D44));
         contactList.setForeground(Color.WHITE);
-        contactList.setSelectionBackground(new Color(0x084D44));
+        contactList.setSelectionBackground(new Color(0xD0D0D0));
         contactList.setSelectionForeground(Color.WHITE);
         contactList.setFont(new Font("Roboto", Font.PLAIN, 14));
 

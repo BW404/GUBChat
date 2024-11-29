a6d175002e2ee2971e87035445f3b10719004672
BW404 <taj.darsana@gmail.com>
2024-11-29 21:22:50 +0600

    Update: 2024-11-29 21:22:50

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        new loginWindow(); // Open the login window
+        SwingUtilities.invokeLater(() -> {
+            new loginWindow(); // Open the login window
+        });
     }
 }

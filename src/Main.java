046510f8a4ee440a4789ed108931c3c6c4db9b73
y4-5H0 <nintuto00@gmail.com>
2024-12-06 20:29:56 +0600

    Refactor loginWindow class name and constructor to lowercase; update references in Main and signupWindow

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
       
         // Launch the login window
-        LoginWindow LoginWindow = new LoginWindow();
+        loginWindow LoginWindow = new loginWindow();
         // Launch the chat window
         // ChatWindow chatWindowhatWindow = new ChatWindow("User"); // Replace "User" with actual username input if needed
 
